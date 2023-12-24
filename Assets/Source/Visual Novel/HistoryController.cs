822871cdd2e9b0ebda0978d2c6504c301fd86e41
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2023-12-24 17:14:48 -0600

    fixed history scrolling

--- Assets/Source/Visual Novel/HistoryController.cs
+++ Assets/Source/Visual Novel/HistoryController.cs
@@ -10,14 +10,24 @@ namespace Source.Visual_Novel
 {
     public class HistoryController : MonoBehaviour
     {
-        public GameObject previousPrefab;
+        public GameObject previousPrefab, holder;
         public Slider slider;
 
         private VisualGameManager _gm;
         private readonly List<GameObject> _previousPrefabs = new ();
         private const int CharacterSpaces = 65;
         private float _previousTextYPos;
-        public float _lastYPos;
+        private float _lastYPos, _holderOrigPosY;
+
+        private void Start()
+        {
+            _holderOrigPosY = holder.transform.position.y;
+        }
+
+        private void OnDisable()
+        {
+            slider.value = slider.minValue;
+        }
 
         public void GenerateHistory()
         {
@@ -29,11 +39,8 @@ namespace Source.Visual_Novel
 
             for (var index = 0; index < previousText.Length; index++)
             {
-                if (Camera.main is null) continue;
-
                 var prev = "";
                 if (index != 0) prev = previousText[index - 1];
-                
                 var obj = Instantiate(previousPrefab, new Vector3(1010, CalYPosition(prev), 0), Quaternion.identity);
                 obj.name = "Previous Text";
                 
@@ -44,26 +51,26 @@ namespace Source.Visual_Novel
                 
                 obj.transform.GetChild(0).GetComponent<TMP_Text>().text += previousText[index];
                 obj.transform.GetChild(1).GetComponent<TMP_Text>().text = names[index] + ": ";
-                obj.transform.SetParent(transform.GetChild(0).GetChild(0));
-
+                obj.transform.SetParent(holder.transform);
+                
                 _previousPrefabs.Add(obj);
                 _lastYPos = obj.transform.position.y;
             }
             
             slider.value = slider.minValue;
-
             SliderMovement();
         }
 
         public void SliderMovement()
         {
-            var yPos = Camera.main.WorldToScreenPoint(new Vector2(0, 0));
-            transform.GetChild(0).GetChild(0).position = new Vector3(transform.GetChild(0).GetChild(0).position.x, yPos.y, 0);
+            var position = holder.transform.position;
+            position = new Vector3(position.x, CalcSliderPos(), 0);
+            holder.transform.position = position;
         }
 
         private float CalcSliderPos()
         {
-            var slope = 0 - (_lastYPos);
+            var slope = -_holderOrigPosY + (-_lastYPos + 500);
             return slope * slider.value;
         }
 
