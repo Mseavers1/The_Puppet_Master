61950d2d7e29f5ce2811ec8699b6d3f554ef639e
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2023-12-24 15:11:49 -0600

    Bug fixing history

--- Assets/Source/Visual Novel/HistoryController.cs
+++ Assets/Source/Visual Novel/HistoryController.cs
@@ -15,19 +15,26 @@ namespace Source.Visual_Novel
 
         private VisualGameManager _gm;
         private readonly List<GameObject> _previousPrefabs = new ();
+        private const int CharacterSpaces = 65;
+        private float _previousTextYPos;
+        public float _lastYPos;
 
         public void GenerateHistory()
         {
             foreach (var prefab in _previousPrefabs) Destroy(prefab);
             
             _previousPrefabs.Clear();
-            var index = 0;
             var names = _gm.GetPreviousNames().ToArray();
-            foreach (var text in _gm.GetPreviousText())
+            var previousText = _gm.GetPreviousText().ToArray();
+
+            for (var index = 0; index < previousText.Length; index++)
             {
                 if (Camera.main is null) continue;
+
+                var prev = "";
+                if (index != 0) prev = previousText[index - 1];
                 
-                var obj = Instantiate(previousPrefab, Camera.main.WorldToScreenPoint(new Vector3(0, -1.5f * index, 0)), Quaternion.identity);
+                var obj = Instantiate(previousPrefab, new Vector3(1010, CalYPosition(prev), 0), Quaternion.identity);
                 obj.name = "Previous Text";
                 
                 var objText = obj.transform.GetChild(0).GetComponent<TMP_Text>();
@@ -35,33 +42,53 @@ namespace Source.Visual_Novel
 
                 for (var i = 0; i < (names[index].Length + 2) * 1.73f; i++) objText.text += " ";
                 
-                obj.transform.GetChild(0).GetComponent<TMP_Text>().text += text;
-                obj.transform.GetChild(1).GetComponent<TMP_Text>().text = names[index++] + ": ";
-                obj.transform.SetParent(transform.GetChild(0));
+                obj.transform.GetChild(0).GetComponent<TMP_Text>().text += previousText[index];
+                obj.transform.GetChild(1).GetComponent<TMP_Text>().text = names[index] + ": ";
+                obj.transform.SetParent(transform.GetChild(0).GetChild(0));
 
                 _previousPrefabs.Add(obj);
+                _lastYPos = obj.transform.position.y;
             }
             
-            slider.maxValue = _previousPrefabs.Count + 8;
-            slider.value = slider.maxValue;
-            
+            slider.value = slider.minValue;
+
             SliderMovement();
         }
 
         public void SliderMovement()
         {
-            var index = 0;
-            foreach (var prefab in _previousPrefabs)
-            {
-                if (Camera.main is null) return;
-                
-                var origPos = Camera.main.WorldToScreenPoint(new Vector3(0, -1.5f * index, 0));
+            var yPos = Camera.main.WorldToScreenPoint(new Vector2(0, 0));
+            transform.GetChild(0).GetChild(0).position = new Vector3(transform.GetChild(0).GetChild(0).position.x, yPos.y, 0);
+        }
 
-                var position = prefab.transform.position;
-                position = new Vector3(position.x, origPos.y + (60 * slider.value), position.z);
-                prefab.transform.position = position;
-                index++;
-            }
+        private float CalcSliderPos()
+        {
+            var slope = 0 - (_lastYPos);
+            return slope * slider.value;
+        }
+
+        private float CalYPosition(string prev)
+        {
+            float yPos;
+            
+            // Check if it is the starting point
+            if (prev.Length == 0) yPos = 500;
+            
+            // Calculate if starting point is already complete
+            else yPos = _previousTextYPos - CalYDifference(prev.Length);
+            
+            _previousTextYPos = yPos;
+            return yPos;
+        }
+
+        private float CalYDifference(int prevCharCount)
+        {
+            return prevCharCount switch
+            {
+                <= CharacterSpaces => 100,
+                <= CharacterSpaces * 2 => 180,
+                _ => 250
+            };
         }
 
         private void Awake()
